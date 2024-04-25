fd0b0fee847f700805bbac19851f8c486fd48b69
tspadua <tiagospadua@gmail.com>
2024-04-25 14:59:22 -0300

    chore: updates on docker compose

---
+++ backend/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'Iniciando setup do projeto...'
+
+echo 'Aguardando conexão com banco de dados...'
+wait4x tcp $POSTGRES_HOST:$POSTGRES_PORT
+echo 'Conexão com banco de dados estabelecida'
+
+npm run migrate 
+
+npm run start:prod
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,6 @@ import { AllExceptionsFilter } from './services/filter/exception-filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
   app.useGlobalFilters(new AllExceptionsFilter(app.get(HttpAdapterHost)));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
